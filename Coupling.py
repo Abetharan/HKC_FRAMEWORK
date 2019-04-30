c08398fbb4eff8ee05b6c09955b8a9dc671e9273
Abetharan <Abetharan09@hotmail.com>
2019-04-30 08:45:38 +0100

    interpolation scheme added to script .. need to add how to do the paths

--- Coupling.py
+++ Coupling.py
@@ -1,16 +1,18 @@
 import os
 import subprocess
-import numpy as np 
 
 os.environ["RUN"]="kappa"
 print(os.environ["RUN"])
 ## laptop
-os.environ["SRCDIR"]=os.environ["HOME"] +"/IMPACT/src"
+os.environ["SRCDIR"]="/Users/shiki/Documents/Imperial_College_London/Ph.D./IMPACT/src"
+#Work station
+# os.environ["SRCDIR"]=os.environ["HOME"] +"/IMPACT/src"
 print(os.environ["SRCDIR"])
 
 os.environ["BASEDIR"]= os.getcwd()
 print(os.environ["BASEDIR"])
   
+
 #-----  Code generation control  -----
 os.environ["FPCODE"]     = "fp2df1_fast"
 os.environ["FPPROF"]   = ""
@@ -19,7 +21,7 @@ os.environ["RUN_ARGS"] = "-log_summary -ksp_monitor -pc_type asm -ksp_type bcgs 
 #-----  Specify behaviour of this run script -os.environ["IS_RESTART
 # os.environ["OVERWRITE_RUN"]
 # os.environ["DONT_COMIPLE"]
-# os.environ["DONT_RUN"]
+os.environ["DONT_RUN"] = ""
 # os.environ["RUN_IN_QUEUE"]  
 
 #------  os.environ["COMPILE-TIME ARRAY DIMENSIONS   ------
@@ -160,9 +162,36 @@ fort14 = """
 file10.write(fort10)
 file12.write(fort12)
 file14.write(fort14)
+os.chdir(os.environ['SRCDIR'])
+os.system('./fp2df1_compile_run.sh')
+#exit()
+from scipy.interpolate import CubicSpline
+import numpy as np 
 
-os.system(os.environ['SRCDIR'] + '/fp2df1_compile_run.sh')
-exit()
+init_file_path = ""
+out_file_path = " "
+fluid_code_path = " "
+no_cycles  = 2
+
+for i in range(0, no_cycles, 1):
+      os.chdir(fluid_code_path)
+      ##need to give some output location 
+      os.system('./run')
+      fluid_x  = np.readtxt(out_file_path)
+      kinetic_x = np.readtxt(kinetic_x_path)
+      fluid_ne = np.readtxt(out_file_path + "/NumberDensityE_" + i + ".txt")
+      fluid_Te = np.readtxt(out_file_path + "/TemperaturE_" + i + ".txt")
+      cs_ne = CubicSpline(fluid_x, fluid_ne)
+      cs_Te = CubicSpline(fluid_x, fluid_Te)
+
+      kinetic_ne = cs_ne(kinetic_x)
+      kinetic_Te = cs_Te(kinetic_x)
+
+      ##### Launch Impact
+      os.chdir(os.environ['BASEDIR'])
+      os.system('mpirun -np $NP ./fp2df1_run')
+
+
 
 
 
