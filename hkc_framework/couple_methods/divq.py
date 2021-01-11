719319b4679bf07bcf5d8336cd0be86d3cc23fc4
Abetharan <Abetharan09@hotmail.com>
2021-01-11 16:45:14 +0100

    added the new files forgot to add

---
+++ hkc_framework/couple_methods/divq.py
@@ -0,0 +1,42 @@
+import numpy as np 
+
+
+
+class DivQ:
+    def method(self, sh_heat_flow, vfp_heat_flow, mass, laser_dir = None, **kwargs):
+        """ Purpose: Find Div.Q
+            Args:
+                electron_thermal_flux = SOL-KiT heat flux in SI
+                mass = areal mass.
+                step = A singel step represent index to next cell-wall. 
+                in the case of SOL-KiT where quantites are defiend in 
+                cell-centres and walls, require a step of two. In
+                HyKiCT quantites are only defined at one point, thus,
+                a step of 1 is sufficient. 
+                
+            Returns: Div.Q
+            NOTE: ONLY WORKS FOR SAME GRIDS
+        """
+        ##Logic relevant only for Limit Density methods 
+        heat_flow = vfp_heat_flow
+        mass = kwargs['mass']
+        #Start limit-density
+        limit_index = len(heat_flow)
+        if laser_dir is not None:
+            if laser_dir == "right":
+                len_sh = len(sh_heat_flow)
+                append_index = len_sh - limit_index + 1
+                heat_flow = heat_flow[1:]
+                heat_flow =  np.append(sh_heat_flow[:append_index], heat_flow)
+            else:
+                heat_flow = heat_flow[:-1]
+                heat_flow = np.append(heat_flow, sh_heat_flow[limit_index - 1:])
+        #End
+
+        nx = len(heat_flow) 
+        self.HeatConductionE = np.zeros(nx - 1)
+        for i, m in enumerate(mass):
+            self.HeatConductionE[i] = (-(heat_flow[i + 1] - heat_flow[i])) / m #- sign is there because of convention used in HyKiCT 
+
+    def setCoupleParams(self, save_path, **kwargs):
+        np.savetxt(os.path.join(save_path,"qe.txt"), self.HeatConductionE)
---
+++ hkc_framework/couple_methods/subtract.py
@@ -0,0 +1,40 @@
+import numpy as np 
+
+
+
+class Subtract:
+    def __init__(self,fluid_yaml, start_kin):
+        raise Exception('Not Implemented')
+
+    def method(self,sh_heat_flow, vfp_heat_flow, laser_dir = None, **kwargs):
+        
+        ##Logic relevant only for Limit Density methods 
+        heat_flow = vfp_heat_flow
+        q_snb = kwargs['q_snb']
+        #Start limit-density
+        limit_index = len(heat_flow)
+        if laser_dir is not None:
+            len_sh = len(sh_heat_flow)
+            if laser_dir == "right":
+                #Remove first term as its 0 
+                heat_flow = heat_flow[1:]
+                append_index = len_sh - limit_index + 1
+                #To retain spitzer-heat in fluid code remove the subtraction factor. 
+                sh_heat_flow[:append_index] = 0 
+                #Pad the VFP heat-flow as it wont be same shape as spitzer-harm
+                padding = len_sh - limit_index + 1
+                heat_flow = np.pad(heat_flow, (padding,0), 'constant', constant_values = (0,0))
+            else:
+                #Remove last term as its 0 
+                heat_flow = heat_flow[:-1]
+                #To retain spitzer-heat in fluid code remove the subtraction factor. 
+                sh_heat_flow[limit_index - 1:] = 0 
+                #Pad the VFP heat-flow as it wont be same shape as spitzer-harm
+                padding = len_sh - limit_index + 1
+                heat_flow = np.pad(heat_flow, (0,padding), 'constant', constant_values = (0,0))
+        #End
+
+        if q_snb is not None:
+            return heat_flow - q_snb
+        else:
+            return heat_flow - sh_heat_flow
